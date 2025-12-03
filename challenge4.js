4d3388b748a3b85ed23dbc985b86ba8091115c2f
zinebsektaoui <zinebsektaoui1@gmail.com>
2025-12-03 12:06:26 +0100

    Calculatrice

---
+++ challenge4.js
@@ -0,0 +1,20 @@
+const p = require('prompt-sync')();
+
+let a = Number(p('Entrer la valeur de a : '));
+let b = Number(p('Entrer la valeur de b : '));
+
+// addition
+console.log('a + b = ', a+b);
+
+// miltiplication
+console.log('a * b = ', a*b);
+
+// soustraction
+console.log('a - b = ', a-b);
+
+// quotient de la division
+console.log('a / b = ', a/b);
+
+// reste de la division
+console.log('a % b = ', a%b);
+
